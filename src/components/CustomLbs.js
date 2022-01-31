1c690cedf620b27dd2fa8c737cfc3d5bcfce2b58
Mendelbaba <mendybaba@gmail.com>
2022-01-31 13:16:10 -0500

    finished cutom lbs

--- src/components/CustomLbs.js
+++ src/components/CustomLbs.js
@@ -34,57 +34,86 @@ function CustomLbs(props) {
     validate,
   });
 
+  const revealElement = (e) => {
+    console.log(e.target.id);
+    if (e.target.id === "squat-changer") {
+      let sq = document.getElementById("custom-lbs-sq");
+      sq.id = "reveal-sq";
+    } else if (e.target.id === "bench-changer") {
+      let bp = document.getElementById("custom-lbs-bp");
+      bp.id = "reveal-bp";
+    } else if (e.target.id === "deaLift-changer") {
+      let dl = document.getElementById("custom-lbs-dl");
+      dl.id = "reveal-dl";
+    }
+
+    // id="custom-lbs-dl"
+  };
+
   return (
     <div>
-      <h2>Custom weights</h2>
+      <h2>enter different values</h2>
+      <button id="squat-changer" onClick={revealElement}>
+        Change squat
+      </button>
+      <button id="bench-changer" onClick={revealElement}>
+        Change bench press
+      </button>
+      <button id="deaLift-changer" onClick={revealElement}>
+        Change deadlift
+      </button>
+
       {/* <label htmlFor="squat"></label>
       <input type="text" id="squat" placeholder="Enter a number" />
       <button onClick={handleClick}>lbs</button> */}
-
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="squat">Squat</label>
-        <input
-          type="number"
-          id="squat"
-          name="squat"
-          min="1"
-          max="1200"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.squat}
-        />
-        <button type="submit">Submit</button>
-      </form>
-
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="bench">Bench</label>
-        <input
-          type="number"
-          id="bench"
-          name="bench"
-          min="1"
-          max="1200"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.bench}
-        />
-        <button type="submit">Submit</button>
-      </form>
-
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="deadlift">Deadlift</label>
-        <input
-          type="number"
-          id="deadlift"
-          name="deadlift"
-          min="1"
-          max="1100"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.deadlift}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <div id="custom-lbs-sq">
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="squat">Squat</label>
+          <input
+            type="number"
+            id="squat"
+            name="squat"
+            min="1"
+            max="1200"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.squat}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div id="custom-lbs-bp">
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="bench">Bench</label>
+          <input
+            type="number"
+            id="bench"
+            name="bench"
+            min="1"
+            max="1200"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.bench}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div id="custom-lbs-dl">
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="deadlift">Deadlift</label>
+          <input
+            type="number"
+            id="deadlift"
+            name="deadlift"
+            min="1"
+            max="1100"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.deadlift}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
